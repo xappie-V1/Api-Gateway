19fd24743d450cfcdf1e5f1984d5e060c595e3df
Sebastian Millahual <seb.millahual@duocuc.cl>
2026-07-10 18:07:22 -0400

    fix: CorsConfig arreglado

--- src/main/java/com/example/innovatech/config/CorsConfig.java
+++ src/main/java/com/example/innovatech/config/CorsConfig.java
@@ -1,26 +1,36 @@
 package com.example.innovatech.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class CorsConfig {
 
     @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins(
-                            "http://localhost:5173",
-                            "https://frontend-dashboard-bay.vercel.app"
-                        )
-                        .allowedMethods("*")
-                        .allowedHeaders("*");
-            }
-        };
+    CorsConfigurationSource corsConfigurationSource() {
+
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.setAllowedOrigins(List.of(
+                "http://localhost:5173",
+                "https://frontend-dashboard-bay.vercel.app"
+        ));
+
+        configuration.setAllowedMethods(List.of("GET","POST","PUT","PATCH","DELETE","OPTIONS"));
+
+        configuration.setAllowedHeaders(List.of("*"));
+
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
     }
 }
